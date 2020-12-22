ee02c825f30f431f01fd031041bf5ecc6960c6c4
joseprm7 <61031221+joseprm7@users.noreply.github.com>
2020-12-22 16:36:11 +0000

    caminhoMaisRapido mas deve estar mal

--- src/dss/armazem/business/ssgestrobots/Mapa.java
+++ src/dss/armazem/business/ssgestrobots/Mapa.java
@@ -2,17 +2,45 @@ package dss.armazem.business.ssgestrobots;
 import java.util.*;
 
 public class Mapa {
-    private final Map<Integer, Collection<Integer>> verticesAdjacentes;
+    private final Map<String, Collection<Node>> grafo;
 
-    public Mapa() {
-        this.verticesAdjacentes = new HashMap<>();
+    public Mapa(Map<String, Collection<Node>> grafo) {
+        this.grafo = grafo;
     }
 
-    public void addNodo(Integer nodo, Collection<Integer> adjacentes) {
-        this.verticesAdjacentes.put(nodo, adjacentes);
+    public void addNodo(String origem, Collection<Node> adjacentes) {
+        this.grafo.put(origem, adjacentes);
     }
 
-    public Collection<Integer> caminhoMaisRapido() {
-        return new ArrayList<>();
+    public int pesoCaminho(Map<String, Integer> caminho) {
+        int peso = 0;
+        for (Map.Entry<String, Integer> entry : caminho.entrySet())
+            peso += entry.getValue();
+        return peso;
+    }
+
+    public Map<String, Integer> caminhoMaisRapido(String origem, String destino) {
+        Collection<Node> nodosAdj = this.grafo.get(origem);
+        Map<String, Integer> caminhoAux, caminhoFinal = new TreeMap<>();
+        caminhoFinal.put(origem, 0);
+        try {
+            for (Node nodo : nodosAdj) {
+                if (nodo.getDestino().equals(destino)) {
+                    caminhoFinal.put(nodo.getDestino(), nodo.getPeso());
+                    return caminhoFinal;
+                } else caminhoAux = caminhoMaisRapido(nodo.getDestino(), destino);
+
+                if (caminhoFinal != null)
+                    caminhoFinal = caminhoAux;
+                else if (pesoCaminho(caminhoAux) < pesoCaminho(caminhoFinal))
+                    caminhoFinal.put(nodo.getDestino(), nodo.getPeso());
+            }
+
+            return caminhoFinal;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new NullPointerException(e.getMessage());
+        }
+
     }
 }
---
+++ src/dss/armazem/business/ssgestrobots/Node.java
@@ -0,0 +1,37 @@
+package dss.armazem.business.ssgestrobots;
+
+public class Node {
+    private String destino;
+    private int peso;
+
+    public Node() {
+        this.destino = "";
+        this.peso = 1;
+    }
+
+    public Node(String destino, int peso) {
+        this.destino = destino;
+        this.peso = peso;
+    }
+
+    public Node(Node n) {
+        this.destino = n.getDestino();
+        this.peso = n.getPeso();
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+}
